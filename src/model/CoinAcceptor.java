8e6705aacabf676b4b4a766cd94f38340c698f4d
Smatov <pitrkirill@gmail.com>
2025-04-18 05:57:05 +0600

    Make CoinAcceptor implement MoneyReceiver

--- src/model/CoinAcceptor.java
+++ src/model/CoinAcceptor.java
@@ -1,17 +1,43 @@
 package model;
 
-public class CoinAcceptor {
+import java.util.Scanner;
+
+public class CoinAcceptor implements MoneyReceiver {
     private int amount;
 
     public CoinAcceptor(int amount) {
         this.amount = amount;
     }
 
+    @Override
     public int getAmount() {
         return amount;
     }
 
+    @Override
     public void setAmount(int amount) {
         this.amount = amount;
+    }
+
+    @Override
+    public void acceptPayment() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите сумму пополнения:");
+        try {
+            int payment = Integer.parseInt(scanner.nextLine());
+            if (payment > 0) {
+                this.amount += payment;
+                System.out.println("Принято " + payment + " монетки");
+            } else {
+                System.out.println("Сумма должна быть положительной");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Сумма некоректная");
+        }
+    }
+
+    @Override
+    public String getCurrencyName() {
+        return "монетки";
     }
 }
